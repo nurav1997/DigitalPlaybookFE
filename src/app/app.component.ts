92565a74ef0ace4fe691466e7ab6b1b7a1d04094
Varun Kulkarni <nurav1997@users.noreply.github.com>
2021-06-14 22:34:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  cardDetails = [
+    {
+      "title": "Coaching",
+      "Description": "The Digital Playbook is similar, but its more than just a reference document. Unlike its paper counterpart, its a dynamic, intelligent system t"
+    },
+    {
+      "title": "Defence",
+      "Description": "The Digital Playbook is similar, but its more than just a reference document. Unlike its paper counterpart, its a dynamic, intelligent system t"
+    },
+    {
+      "title": "Offence",
+      "Description": "The Digital Playbook is similar, but its more than just a reference document. Unlike its paper counterpart, its a dynamic, intelligent system t"
+    },
+    {
+      "title": "Management",
+      "Description": "The Digital Playbook is similar, but its more than just a reference document. Unlike its paper counterpart, its a dynamic, intelligent system t"
+    },
+    {
+      "title": "Strength",
+      "Description": "The Digital Playbook is similar, but its more than just a reference document. Unlike its paper counterpart, its a dynamic, intelligent system t"
+    }
+  ];
+
+  lastCardDetails = [{
+    "title": "Information Architecture"
+  },
+  {
+    "title": "Personas"
+  },
+  {
+    "title": "User Journey"
+  },
+  {
+    "title": "Content Strategy"
+  },
+  {
+    "title": "Wireframe"
+  },
+  {
+    "title": "Prototype"
+  },
+  {
+    "title": "Website Implementation"
+  },
+  {
+    "title": "Analytics"
+  }];
   title = 'digitalplaybooklanding';
+  ngOnInit() {
+    AOS.init();
+  }
 }
