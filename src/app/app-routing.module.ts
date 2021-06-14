fd533b285dba5e1a8ee2c0ae7124f60884847292
Varun Kulkarni <nurav1997@users.noreply.github.com>
2021-06-14 23:11:03 +0530

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/landing', pathMatch: 'full'
+  },
+  {
+    path: "login", component: AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
